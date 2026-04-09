3917fd9964c3b7e0506d73fe13c701fe6aaf22ca
Eric Wooley <1930227+ericwooley@users.noreply.github.com>
2026-04-09 15:47:54 -0600

    Normalize ssh-man cask release metadata

--- Casks/ssh-man.rb
+++ Casks/ssh-man.rb
@@ -2,15 +2,15 @@ cask "ssh-man" do
   version "1.0.0"
   sha256 "REPLACE_WITH_SHA256_HASH"
 
-  url "https://github.com/ericwooley/ssh-man/releases/download/v#{version}/myapp.dmg"
+  url "https://github.com/ericwooley/ssh-man/releases/download/v#{version}/ssh-man.dmg"
   name "SSH Man"
-  desc "Manage your ssh tunnels and socks5 proxies."
+  desc "Manage your SSH tunnels and SOCKS5 proxies."
   homepage "https://github.com/ericwooley/ssh-man"
 
   app "ssh-man.app"
 
   zap trash: [
     "~/Library/Application Support/ssh-man",
-    "~/Library/Preferences/tech.moonpixels.ssh-man.plist",
+    "~/Library/Preferences/com.wails.ssh-man.plist",
   ]
 end
