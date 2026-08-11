36fd83dda4efb9a19d25e43ff4fef1c7271ed8de
Eric Wooley <1930227+ericwooley@users.noreply.github.com>
2026-08-11 19:44:57 +0000

    chore: update proc-man Formula to v1.1.6

--- Formula/proc-man.rb
+++ Formula/proc-man.rb
@@ -3,28 +3,28 @@
 class ProcMan < Formula
   desc "Manage local development processes and logs"
   homepage "https://github.com/ericwooley/proc-man"
-  version "1.1.5"
+  version "1.1.6"
   license "MIT"
 
   on_macos do
     on_intel do
       url "https://github.com/ericwooley/proc-man/releases/download/v#{version}/proc-man_#{version}_darwin_amd64.tar.gz"
-      sha256 "f8015f8008aae222b8889cd44fc5ea30a96fe06504539da1b4cc0ff5fdebb8cd"
+      sha256 "c0ed303f56252e2210b07323485d05d4d6704017c81906a0b78e72bfd7bee402"
     end
     on_arm do
       url "https://github.com/ericwooley/proc-man/releases/download/v#{version}/proc-man_#{version}_darwin_arm64.tar.gz"
-      sha256 "6caa2aefdd404add70edd2d38e416e250b8f6eb65a104d6d5b41ef3e20d17838"
+      sha256 "fe1932aab93c4ebc39de8d3b5bde7c0f4e2304ace2d7934b2eac00d990de765c"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/ericwooley/proc-man/releases/download/v#{version}/proc-man_#{version}_linux_amd64.tar.gz"
-      sha256 "0aa9528d82da90c46c51863f1be007459e1a81f59751ca39489eb17338beb3da"
+      sha256 "347051a105d7c61742ab7d29325de6db88c3c8d7179f579d3c08cf0e059564a5"
     end
     on_arm do
       url "https://github.com/ericwooley/proc-man/releases/download/v#{version}/proc-man_#{version}_linux_arm64.tar.gz"
-      sha256 "998fc90851ea8a923c7dfd5a714938866830d55b925b301a21d2b4e72f0a77c6"
+      sha256 "4d4d334aadcc74a1a8beda0d349562a2a790923c8a01c39528a71cd74d13d96b"
     end
   end
 
@@ -32,13 +32,10 @@ class ProcMan < Formula
     bin.install "proc-man"
   end
 
-  def post_install
-    system bin/"proc-man", "daemon", "install", "--now"
-  end
-
   def caveats
     <<~EOS
-      proc-man installs and starts its user service during installation.
+      Start or update the proc-man user service after installation or upgrade:
+        proc-man daemon install --now
       Run `proc-man daemon uninstall` before `brew uninstall proc-man`.
     EOS
   end
