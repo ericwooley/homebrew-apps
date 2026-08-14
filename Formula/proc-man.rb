eec7efe3e1fa9d440c76c1ae90c5a3efccdd66c9
Eric Wooley <1930227+ericwooley@users.noreply.github.com>
2026-08-14 16:35:33 +0000

    chore: update proc-man Formula to v1.2.1

--- Formula/proc-man.rb
+++ Formula/proc-man.rb
@@ -3,28 +3,28 @@
 class ProcMan < Formula
   desc "Manage local development processes and logs"
   homepage "https://github.com/ericwooley/proc-man"
-  version "1.2.0"
+  version "1.2.1"
   license "MIT"
 
   on_macos do
     on_intel do
       url "https://github.com/ericwooley/proc-man/releases/download/v#{version}/proc-man_#{version}_darwin_amd64.tar.gz"
-      sha256 "eccb3af26438c8fea663f53576c2c9610126b7786b7dfb94cd90608be4f20521"
+      sha256 "6b6e89571e423085fa8d19b5e0a8016229bff0a3f643802631c4c42a17b8066b"
     end
     on_arm do
       url "https://github.com/ericwooley/proc-man/releases/download/v#{version}/proc-man_#{version}_darwin_arm64.tar.gz"
-      sha256 "1082446a949de640e330d6e5d8b1e1238efb3981ebab5f831f300d9167c8485a"
+      sha256 "a62c7bae4105ff6d5e23aa1473e8516269b28cfd2fa1cbea969501b577075ff8"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/ericwooley/proc-man/releases/download/v#{version}/proc-man_#{version}_linux_amd64.tar.gz"
-      sha256 "e7d2c69da1e579ceb93af0d24eb5e3277c9fad0e1d8a48a62974db987931d1f6"
+      sha256 "a76671c92d895e0cb1a4f35055eccf00deec588927f504dd633d945ed3e3003e"
     end
     on_arm do
       url "https://github.com/ericwooley/proc-man/releases/download/v#{version}/proc-man_#{version}_linux_arm64.tar.gz"
-      sha256 "bad25b8f33f209f35a69a79f2789415518c784801c829078cc696f660df86060"
+      sha256 "7510840b952ad6591f51834cdbaea6bee8c809078d72d219b37e5baa40f41c0a"
     end
   end
 
